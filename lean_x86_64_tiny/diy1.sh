1567d23d49ae4e228fe7bdacf8889eac770216a9
aaronbagot <61593403+aaronbagot@users.noreply.github.com>
2020-06-06 00:02:37 +0800

    Add files via upload

--- lean_x86_64_tiny/diy1.sh
+++ lean_x86_64_tiny/diy1.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+mkdir -p package/custom
+cd package/custom
+# git clone https://github.com/coolsnowwolf/packages
+git clone https://github.com/kenzok8/small
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/Lienol/openwrt-package
+#git clone https://github.com/fw876/helloworld
+#git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+#git clone https://github.com/vernesong/OpenClash
+#git clone https://github.com/tty228/luci-app-serverchan
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome
+#git clone https://github.com/pymumu/luci-app-smartdns
+#git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+cd ../../
