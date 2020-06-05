ce0c9279a52cc5e00176122974b3e24599f09d4b
aaronbagot <61593403+aaronbagot@users.noreply.github.com>
2020-06-05 22:03:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-#=============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=============================================================
-
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+mkdir -p package/custom
+cd package/custom
+# git clone https://github.com/coolsnowwolf/packages
+git clone https://github.com/kenzok8/small
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/Lienol/openwrt-package
+git clone https://github.com/fw876/helloworld
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+git clone https://github.com/vernesong/OpenClash
+git clone https://github.com/tty228/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone https://github.com/pymumu/luci-app-smartdns
+git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+cd ../../
+
+# Modify default IP
+
+sed -i 's/192.168.1.1/192.168.1.201/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+# Add some default settings
+
+#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
+
