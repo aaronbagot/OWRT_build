8d2868be2bc618a9c33ed73aba0890480d666a4d
dfht-cao <58374544+dfht-cao@users.noreply.github.com>
2020-06-07 14:03:28 +0800

    offfical version unfinished

--- Official19.07/diy2.sh
+++ Official19.07/diy2.sh
@@ -3,3 +3,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.202/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+# add more in tools: Makefile, uci, upx
+[ -e files/toolsmore.tar.gz ] && tar -xzvf files/toolsmore.tar.gz
