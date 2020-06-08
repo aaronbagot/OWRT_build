b7b8a4bd1dca5079dfa506ee79d9e3e875ac8710
dfht-cao <58374544+dfht-cao@users.noreply.github.com>
2020-06-08 18:12:02 +0800

    no shadow clone

--- lean_x86_64_mini/diy1.sh
+++ lean_x86_64_mini/diy1.sh
@@ -6,19 +6,17 @@
 
 mkdir -p package/custom
 cd package/custom
-# git clone --depth 1 https://github.com/coolsnowwolf/packages leanpackage && echo 'leanpackage' > leanpackage/version
-#git clone --depth 1 https://github.com/kenzok8/small passwallneeded && echo 'passwallneeded' > passwallneeded/version
-#git clone --depth 1 https://github.com/kenzok8/openwrt-packages deletedpackage && echo 'deletedpackage' > deletedpackage/version
-#git clone --depth 1 https://github.com/Lienol/openwrt-package lienolpackage && echo 'lienolpackage' > lienolpackage/version
-#git clone --depth 1 https://github.com/fw876/helloworld helloworld && echo 'helloworld' > helloworld/version
-#git clone --depth 1 https://github.com/openwrt-develop/luci-theme-atmaterial atmaterial && echo 'atmaterial' > atmaterial/version
-#git clone --depth 1 https://github.com/vernesong/OpenClash OpenClash && echo 'OpenClash' > OpenClash/version
-#git clone --depth 1 https://github.com/tty228/luci-app-serverchan serverchan && echo 'serverchan' > serverchan/version
-#git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome adguardhome && echo 'adguardhome' > adguardhome/version
-#git clone --depth 1 https://github.com/pymumu/luci-app-smartdns smartdns && echo 'smartdns' > smartdns/version
-#git clone --depth 1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon argon &&echo 'argon' > argon/version
-
+#git clone https://github.com/coolsnowwolf/packages leanpackage
 git clone https://github.com/kenzok8/small passwallneeded
 git clone https://github.com/kenzok8/openwrt-packages deletedpackage
 git clone https://github.com/Lienol/openwrt-package lienolpackage
+#git clone https://github.com/fw876/helloworld helloworld
+#git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+#git clone https://github.com/vernesong/OpenClash OpenClash
+#git clone https://github.com/tty228/luci-app-serverchan
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome
+#git clone https://github.com/pymumu/luci-app-smartdns smartdns
+#git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+cd ../..
+
 
