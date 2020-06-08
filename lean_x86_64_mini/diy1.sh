10703101441055487cf9bd1e969c04902f1ac6f5
dfht-cao <58374544+dfht-cao@users.noreply.github.com>
2020-06-08 15:02:45 +0800

    try shadow copies

--- lean_x86_64_mini/diy1.sh
+++ lean_x86_64_mini/diy1.sh
@@ -7,9 +7,9 @@
 mkdir -p package/custom
 cd package/custom
 # git clone --depth 1 https://github.com/coolsnowwolf/packages leanpackage && echo 'leanpackage' > leanpackage/version
-git clone --depth 1 https://github.com/kenzok8/small passwallneeded && echo 'passwallneeded' > passwallneeded/version
-git clone --depth 1 https://github.com/kenzok8/openwrt-packages deletedpackage && echo 'deletedpackage' > deletedpackage/version
-git clone --depth 1 https://github.com/Lienol/openwrt-package lienolpackage && echo 'lienolpackage' > lienolpackage/version
+#git clone --depth 1 https://github.com/kenzok8/small passwallneeded && echo 'passwallneeded' > passwallneeded/version
+#git clone --depth 1 https://github.com/kenzok8/openwrt-packages deletedpackage && echo 'deletedpackage' > deletedpackage/version
+#git clone --depth 1 https://github.com/Lienol/openwrt-package lienolpackage && echo 'lienolpackage' > lienolpackage/version
 #git clone --depth 1 https://github.com/fw876/helloworld helloworld && echo 'helloworld' > helloworld/version
 #git clone --depth 1 https://github.com/openwrt-develop/luci-theme-atmaterial atmaterial && echo 'atmaterial' > atmaterial/version
 #git clone --depth 1 https://github.com/vernesong/OpenClash OpenClash && echo 'OpenClash' > OpenClash/version
@@ -17,3 +17,8 @@ git clone --depth 1 https://github.com/Lienol/openwrt-package lienolpackage && e
 #git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome adguardhome && echo 'adguardhome' > adguardhome/version
 #git clone --depth 1 https://github.com/pymumu/luci-app-smartdns smartdns && echo 'smartdns' > smartdns/version
 #git clone --depth 1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon argon &&echo 'argon' > argon/version
+
+git clone https://github.com/kenzok8/small passwallneeded
+git clone https://github.com/kenzok8/openwrt-packages deletedpackage
+git clone https://github.com/Lienol/openwrt-package lienolpackage
+
