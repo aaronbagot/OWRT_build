87efa6e600655effdd1917d1029aad17c3393040
aaronbagot <61593403+aaronbagot@users.noreply.github.com>
2021-03-09 02:07:01 +0800

    03090001

--- lean_x86_64_mini/diy1.sh
+++ lean_x86_64_mini/diy1.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-mkdir -p package/custom
-cd package/custom
-#git clone https://github.com/coolsnowwolf/packages leanpackage
-git clone https://github.com/kenzok8/small passwallneeded
-git clone https://github.com/kenzok8/openwrt-packages deletedpackage
-git clone https://github.com/Lienol/openwrt-package lienolpackage
-#git clone https://github.com/fw876/helloworld helloworld
-#git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-#git clone https://github.com/vernesong/OpenClash OpenClash
-#git clone https://github.com/tty228/luci-app-serverchan
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#git clone https://github.com/pymumu/luci-app-smartdns smartdns
-#git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-cd ../..
+# 方案一：clone kenzok8，然后删掉其passwall, 加上新版的passwall
+# the following for lean's openwrt master only
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+
+cd package
+
+git clone https://github.com/kenzok8/openwrt-packages kenzok8
+rm -rf kenzok8/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall kenzok8/luci-app-passwall
+cd ..
+# the following for lean with luci-app-bypass
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 
