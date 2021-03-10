254af8038e4ffdf2f605bae5c1294d9ba7bd09e4
aaronbagot <61593403+aaronbagot@users.noreply.github.com>
2021-03-11 03:04:29 +0800

    0311002

--- lean_x86_64_mini/diy1.sh
+++ lean_x86_64_mini/diy1.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# 方案一：clone kenzok8，然后删掉其passwall, 加上新版的passwall
 # the following for lean's openwrt master only
 # sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 cd package
-
 git clone https://github.com/kenzok8/openwrt-packages kenzok8
-rm -rf kenzok8/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall kenzok8/luci-app-passwall
+cd kenzok8
+for f in gost luci-app-clash luci-app-dnsfilter luci-app-gost luci-app-openclash luci-app-passwall luci-app-smartdns luci-app-ssr-plus luci-app-vssr luci-theme-argon_new luci-theme-atmaterial luci-theme-edge luci-theme-opentomato luci-theme-opentomcat luci-theme-opentopd screenshot smartdns ; do rm -rf ${f} ; done
 cd ..
-# the following for lean with luci-app-bypass
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+git clone https://github.com/Lienol/openwrt-package lienolpkg
+cd lienolpkg
+shopt -s extglob
+rm -rf !(*control*)  || true
+shopt -s nullglob
+cd ..
+git clone https://github.com/xiaorouji/openwrt-passwall passwall 
+cd ..
 
 
