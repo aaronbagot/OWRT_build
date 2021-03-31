0575e48464786aa68604f9845b39c316a3048f5b
aaronbagot <61593403+aaronbagot@users.noreply.github.com>
2021-03-31 09:20:45 +0800

    20210331

--- lean_x86_64_mini/diy1.sh
+++ lean_x86_64_mini/diy1.sh
@@ -3,19 +3,22 @@
 # the following for lean's openwrt master only
 # sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
-cd package
-git clone https://github.com/xiaorouji/openwrt-passwall passwall 
+echo "lean_R2x_small" > lean/version
+mkdir -p package/custom
+cd package/custom
+git clone https://github.com/xiaorouji/openwrt-passwall passwall --depth 1
 mkdir -p kenzok8
 cd kenzok8
-for f in AdGuardHome lua-maxminddb luci-app-adguardhome luci-app-advancedsetting luci-app-aliddns luci-app-eqos luci-app-jd-dailybonus luci-theme-ifit microsocks naiveproxy redsocks2 tcping
-	do 
-		svn export --quiet https://github.com/kenzok8/openwrt-packages/trunk/${f}
-done
-cd ..
-mkdir -p lienolpkg
-cd lienolpkg
+svn export --quiet https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
+svn export --quiet https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
+svn export --quiet https://github.com/kenzok8/openwrt-packages/trunk/luci-app-advancedsetting
+svn export --quiet https://github.com/kenzok8/openwrt-packages/trunk/luci-app-aliddns
+svn export --quiet https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos 
+# lua-maxminddb microsocks naiveproxy redsocks2 tcping
+mkdir -p ../lienolpkg
+cd ../lienolpkg
 svn export --quiet https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol
 svn export --quiet https://github.com/Lienol/openwrt-package/trunk/luci-app-control-webrestriction
 svn export --quiet https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl
 svn export --quiet https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol
-cd ../..
+cd ../../..
